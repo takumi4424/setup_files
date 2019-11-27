9d2f212ca282684b2d12338a3dd2656de876559c
Takumi Kodama <kodamat@makino.co.jp>
2019-11-27 14:02:22 +0900

    [add] VSCode installation function

--- setup.sh
+++ setup.sh
@@ -101,6 +101,19 @@ yes_or_no 'setup prompt?' || {
     done
 }
 
+line_sep
+yes_or_no 'install VSCode?' || {
+    if [ -n "$(which code)" ]; then
+        echo 'VSCode is already installed.'
+    else
+        curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > ./microsoft.gpg
+        sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+        sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+        sudo apt update
+        sudo apt install -y code
+    fi
+}
+
 if [ -f /opt/ros/melodic/setup.bash ]; then
     echo '# ROS-melodic settings' >> $bashrc
     echo 'source /opt/ros/melodic/setup.bash' >> $bashrc
